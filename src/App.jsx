253133b9eadad60ba36e1d02109d1d890d72efb4
Kalpesh Pagar <kalpeshpagar57@gmail.com>
2026-01-10 22:32:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import SimpleCalculator from './components/SimpleCalculator'
+import CharacterCounterApp from './components/CharacterCounterApp'
+import BackgroundGradientGenarator from './components/BackgroundGradientGenarator'
+import DigitalClock from './components/DigitalClock'
+
+const App = () => {
+  return (
+    <div>
+      {/* <CharacterCounterApp/>
+      <SimpleCalculator /> */}
+      {/* <BackgroundGradientGenarator/> */}
+      <DigitalClock/>
+    </div>
+  )
+}
+
+export default App
